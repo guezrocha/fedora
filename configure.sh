842430bc4eebb83f0cb3f6b0e710da3d77223479
guezrocha <guezrocha@gmail.com>
2025-11-14 20:24:12 +0100

    Increase cursor size, increase font size

---
+++ configure.sh
@@ -0,0 +1,7 @@
+# Increase cursor size while keeping it sharp
+gsettings set org.gnome.desktop.interface cursor-size 36
+
+# Increase font size to 14
+gsettings set org.gnome.desktop.interface font-name 'Adwaita Sans 14'
+gsettings set org.gnome.desktop.interface document-font-name 'Adwaita Sans 14'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Adwaita Mono 14'

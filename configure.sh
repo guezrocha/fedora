58e01132b857034ea9ff90bb152f848a0a9626d6
guezrocha <guezrocha@gmail.com>
2025-11-14 20:52:53 +0100

    Disable hinting and antialising

--- configure.sh
+++ configure.sh
@@ -5,3 +5,10 @@ gsettings set org.gnome.desktop.interface cursor-size 36
 gsettings set org.gnome.desktop.interface font-name 'Adwaita Sans 14'
 gsettings set org.gnome.desktop.interface document-font-name 'Adwaita Sans 14'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Adwaita Mono 14'
+
+# Uninstall software
+sudo dnf remove -y libreoffice*
+
+# Disable hinting and antialising
+gsettings set org.gnome.desktop.interface font-hinting 'none'
+gsettings set org.gnome.desktop.interface font-antialiasing 'none'

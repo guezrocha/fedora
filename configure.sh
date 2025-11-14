cb90714c22eb676f55455ac64b64c38a70704b4f
guezrocha <guezrocha@gmail.com>
2025-11-14 21:11:25 +0100

    Remove Fedora watermark

--- configure.sh
+++ configure.sh
@@ -12,3 +12,7 @@ sudo dnf remove -y libreoffice*
 # Configure hinting and antialising
 # gsettings set org.gnome.desktop.interface font-hinting 'full'
 # gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
+
+# Remove Fedora watermark
+# gnome-extensions list
+# gnome-extensions uninstall fedora-watermark@gnome-shell.fedoraproject.org
